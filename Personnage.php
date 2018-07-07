fd0b1bd671a733bab16655c5a003b8742b08146e
Maxime Guillemot <guillemot.maxime@gmail.com>
2018-07-07 16:37:24 +0200

    Amélioration des setters.

--- Personnage.php
+++ Personnage.php
@@ -1,29 +1,59 @@
 <?php
 class Personnage
 {
-    private $_force = 50;
-    private $_localication = 'Lyon';
-    private $_experience = 1;
-    private $_degats = 0;
+    private $_force;
+    private $_experience;
+    private $_degats;
 
     // Mutateurs - Setters
     public function setForce($force)
     {
-        $this->_force = $force;
-    }
+        if(!is_int($force))
+        {
+            trigger_error('La force d\' un personnage doit être un nombre entier.', E_USER_WARNING);
+            return;
+        }
 
-    public function setLocalisation($localisation)
-    {
-        $this->_localisation = $localisation;
+        if($force < 0 || $force > 100)
+        {
+            trigger_error('La force doit être comprise entre 0 et 100.', E_USER_WARNING);
+            return;
+        }
+        
+        $this->_force = $force;
     }
 
     public function setExperience($experience)
     {
+        if(!is_int($experience))
+        {
+            trigger_error('L\'expérience doit être un nombre entier.', E_USER_WARNING);
+            return;
+        }
+
+        if($experience < 0 || $experience > 100)
+        {
+            trigger_error('L\'expérience doit être comprise entre 0 et 100.', E_USER_WARNING);
+            return;
+        }
+
         $this->_experience = $experience;
     }
 
     public function setDegats($degats)
     {
+        if(!is_int($degats))
+        {
+            trigger_error('La valeur de dégâts doit un être un nombre entier.', E_USER_WARNING);
+            return;
+        }
+
+        if($degats < 0 || $degats > 100)
+        {
+            trigger_error('Les dégâts doivent être compris entre 0 et 100.', E_USER_WARNING);
+            return;
+        }
+
         $this->_degats = $degats;
     }
 
