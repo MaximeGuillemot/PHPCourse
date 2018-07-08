46207f18e90cc4de7012c39e35438738805922b6
Maxime Guillemot <guillemot.maxime@gmail.com>
2018-07-08 16:26:53 +0200

    Ajout d'un manager permettant de gérer les personnages avec la BDD.

--- index.php
+++ index.php
@@ -27,4 +27,18 @@ Personnage::parler();
 
 echo Personnage::statique();
 
+
+$perso3 = new Personnage([
+    'nom' => 'Victor',
+    'forcePerso' => 5,
+    'degats' => 0,
+    'niveau' => 1,
+    'experience' => 0
+  ]);
+      
+  $db = new PDO('mysql:host=localhost;dbname=tests', 'root', '');
+  
+  $manager = new PersonnagesManager($db);
+  $manager->add($perso);
+
 ?>
